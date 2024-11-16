4f35529bdd745b122c490b6e02931cf4537b9341
ScubaScripter <145914099+YamiMitsuha@users.noreply.github.com>
2024-11-17 00:08:29 +0800

    Add files via upload

---
+++ 05 Laboratory Exercise 1 - ARG/FrmLab1/FrmLab1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace FrmLab1
+{
+    partial class FrmLab1
+    {
+        // Paala, James P.            BSIT - 501
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <param name="disposing"
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            txtInput = new TextBox();
+            btnCreate = new Button();
+            SuspendLayout();
+
+            txtInput.Location = new Point(81, 42);
+            txtInput.Multiline = true;
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(250, 121);
+            txtInput.TabIndex = 3;
+
+            btnCreate.Location = new Point(149, 208);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(110, 35);
+            btnCreate.TabIndex = 4;
+            btnCreate.Text = "Create";
+            btnCreate.UseVisualStyleBackColor = true;
+            btnCreate.Click += btnCreate_Click;
+
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(418, 283);
+            Controls.Add(btnCreate);
+            Controls.Add(txtInput);
+            Name = "FrmLab1";
+            Text = "FrmLab1";
+            Load += FrmLab1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox txtInput;
+        private Button btnCreate;
+    }
+}
